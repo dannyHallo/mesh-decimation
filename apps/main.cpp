a6262921abee63750848c9b7f5416bd9f52228ad
danny <ruitian0716@gmail.com>
2024-04-07 14:14:55 +0800

    Finished tutorial

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,9 +1,13 @@
 #include "application/Application.hpp"
 
+#include <iostream>
+
 int main() {
   Application app{};
 
+  std::cout << "running application...\n";
   app.run();
+  std::cout << "application finished.\n";
 
   return 0;
 }
--- src/application/Application.cpp
+++ src/application/Application.cpp
@@ -6,9 +6,94 @@
 #include <daxa/utils/pipeline_manager.hpp>
 #include <daxa/utils/task_graph.hpp>
 
+#include "shared/PushConstants.inl"
+
 #include <iostream>
 
 using namespace daxa::types;
+
+struct UploadVertexDataTask {
+  struct Uses {
+    daxa::BufferTransferWrite vertex_buffer{};
+  } uses = {};
+
+  std::string_view name = "upload vertices";
+
+  void callback(daxa::TaskInterface ti) {
+    auto commandList = ti.get_command_list();
+    auto data = std::array{
+        MyVertex{.position = {-0.5f, +0.5f, 0.0f}, .color = {1.0f, 0.0f, 0.0f}},
+        MyVertex{.position = {+0.5f, +0.5f, 0.0f}, .color = {0.0f, 1.0f, 0.0f}},
+        MyVertex{.position = {+0.0f, -0.5f, 0.0f}, .color = {0.0f, 0.0f, 1.0f}},
+    };
+
+    auto staging_buffer_id = ti.get_device().create_buffer({
+        .size = sizeof(data),
+        .allocate_info = daxa::MemoryFlagBits::HOST_ACCESS_RANDOM,
+        .name = "my staging buffer",
+    });
+
+    commandList.destroy_buffer_deferred(staging_buffer_id);
+
+    auto *buffer_ptr =
+        ti.get_device().get_host_address_as<std::array<MyVertex, 3>>(
+            staging_buffer_id);
+
+    *buffer_ptr = data;
+    commandList.copy_buffer_to_buffer({
+        .src_buffer = staging_buffer_id,
+        .dst_buffer = uses.vertex_buffer.buffer(),
+        .size = sizeof(data),
+    });
+  }
+};
+
+struct DrawToSwapchainTask {
+  struct Uses {
+    // We declare a vertex buffer read. Later we assign the task vertex buffer
+    // handle to this use.
+    daxa::BufferVertexShaderRead vertex_buffer{};
+    // We declare a color target. We will assign the swapchain task image to
+    // this later. The name `ImageColorAttachment<T_VIEW_TYPE = DEFAULT>` is a
+    // typedef for
+    // `daxa::TaskImageUse<daxa::TaskImageAccess::COLOR_ATTACHMENT,
+    // T_VIEW_TYPE>`.
+    daxa::ImageColorAttachment<> color_target{};
+  } uses = {};
+
+  daxa::RasterPipeline *pipeline = {};
+
+  std::string_view name = "draw task";
+
+  void callback(daxa::TaskInterface ti) {
+    auto commandList = ti.get_command_list();
+
+    auto const size_x =
+        ti.get_device().info_image(uses.color_target.image()).size.x;
+    auto const size_y =
+        ti.get_device().info_image(uses.color_target.image()).size.y;
+
+    commandList.begin_renderpass({
+        .color_attachments =
+            std::vector{
+                daxa::RenderAttachmentInfo{
+                    .image_view = uses.color_target.view(),
+                    .load_op = daxa::AttachmentLoadOp::CLEAR,
+                    .clear_value =
+                        std::array<daxa::f32, 4>{0.1f, 0.0f, 0.5f, 1.0f},
+                },
+            },
+        .render_area = {.x = 0, .y = 0, .width = size_x, .height = size_y},
+    });
+
+    commandList.set_pipeline(*pipeline);
+    commandList.push_constant(MyPushConstant{
+        .my_vertex_ptr =
+            ti.get_device().get_device_address(uses.vertex_buffer.buffer())});
+    commandList.draw({.vertex_count = 3});
+    commandList.end_renderpass();
+  }
+};
 
 Application::Application() {
   // Create a window
@@ -16,10 +101,6 @@ Application::Application() {
 
   // Daxa code goes here...
   daxa::Instance instance = daxa::create_instance({});
-
-  while (!window.shouldClose()) {
-    window.update();
-  }
 
   daxa::Device device = instance.create_device({
       .selector = [](daxa::DeviceProperties const &device_props) -> daxa::i32 {
@@ -44,9 +125,9 @@ Application::Application() {
       .name = "my device",
   });
 
-  daxa::Swapchain swapchain = device.create_swapchain({
+  daxa::Swapchain swapchain = device.create_swapchain(daxa::SwapchainInfo{
       // this handle is given by the windowing API
-      .native_window = nullptr,
+      .native_window = window.getNativeHandle(),
 
       // The platform would also be retrieved from the windowing API,
       // or by hard-coding it depending on the OS.
@@ -69,6 +150,128 @@ Application::Application() {
       .image_usage = daxa::ImageUsageFlagBits::TRANSFER_DST,
       .name = "my swapchain",
   });
+
+  auto pipeline_manager = daxa::PipelineManager({
+      .device = device,
+      .shader_compile_options =
+          {
+              .root_paths =
+                  {
+                      DAXA_SHADER_INCLUDE_DIR,
+                      "C:/Users/danny/Desktop/mesh-decimation/src/shaders",
+                      "C:/Users/danny/Desktop/mesh-decimation/src/shared",
+                  },
+              .language = daxa::ShaderLanguage::GLSL,
+              .enable_debug_info = true,
+          },
+      .name = "my pipeline manager",
+  });
+
+  std::shared_ptr<daxa::RasterPipeline> pipeline;
+  {
+    auto result = pipeline_manager.add_raster_pipeline({
+        .vertex_shader_info =
+            daxa::ShaderCompileInfo{.source = daxa::ShaderFile{"main.glsl"}},
+        .fragment_shader_info =
+            daxa::ShaderCompileInfo{.source = daxa::ShaderFile{"main.glsl"}},
+        .color_attachments = {{.format = swapchain.get_format()}},
+        .raster = {},
+        .push_constant_size = sizeof(MyPushConstant),
+        .name = "my pipeline",
+    });
+    if (result.is_err()) {
+      std::cerr << result.message() << std::endl;
+      return;
+    }
+    pipeline = result.value();
+  }
+
+  auto buffer_id = device.create_buffer({
+      .size = sizeof(MyVertex) * 3,
+      .name = "my vertex data",
+  });
+
+  auto task_swapchain_image =
+      daxa::TaskImage{{.swapchain_image = true, .name = "swapchain image"}};
+
+  auto task_vertex_buffer = daxa::TaskBuffer({
+      .initial_buffers = {.buffers = std::span{&buffer_id, 1}},
+      .name = "task vertex buffer",
+  });
+
+  auto loop_task_graph = daxa::TaskGraph({
+      .device = device,
+      .swapchain = swapchain,
+      .name = "loop",
+  });
+
+  loop_task_graph.use_persistent_buffer(task_vertex_buffer);
+  loop_task_graph.use_persistent_image(task_swapchain_image);
+
+  loop_task_graph.add_task(DrawToSwapchainTask{
+      .uses =
+          {
+              .vertex_buffer = task_vertex_buffer.view(),
+              .color_target = task_swapchain_image.view(),
+          },
+      .pipeline = pipeline.get(),
+  });
+
+  loop_task_graph.submit({});
+  // And tell the task graph to do the present step.
+  loop_task_graph.present({});
+  // Finally, we complete the task graph, which essentially compiles the
+  // dependency graph between tasks, and inserts the most optimal
+  // synchronization!
+  loop_task_graph.complete({});
+
+  // creating a vertex uploading graph
+  {
+    auto upload_task_graph = daxa::TaskGraph({
+        .device = device,
+        .name = "upload",
+    });
+
+    upload_task_graph.use_persistent_buffer(task_vertex_buffer);
+
+    upload_task_graph.add_task(UploadVertexDataTask{
+        .uses =
+            {
+                .vertex_buffer = task_vertex_buffer.view(),
+            },
+    });
+
+    upload_task_graph.submit({});
+    upload_task_graph.complete({});
+
+    upload_task_graph.execute({});
+  }
+
+  while (!window.shouldClose()) {
+    window.update();
+
+    if (window.swapchain_out_of_date) {
+      swapchain.resize();
+      window.swapchain_out_of_date = false;
+    }
+
+    // acquire the next image
+    auto swapchain_image = swapchain.acquire_next_image();
+    if (swapchain_image.is_empty()) {
+      continue;
+    }
+
+    // We update the image id of the task swapchain image.
+    task_swapchain_image.set_images({.images = std::span{&swapchain_image, 1}});
+
+    // So, now all we need to do is execute our task graph!
+    loop_task_graph.execute({});
+    device.collect_garbage();
+  }
+
+  device.wait_idle();
+  device.collect_garbage();
+  device.destroy_buffer(buffer_id);
 }
 
 Application::~Application() = default;
